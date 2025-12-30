1cef7a55f997498b71ad968201095c9c0c7857b0
nakasita <kosei0224.loop@icloud.com>
2025-12-30 23:00:00 +0900

    url test

--- src/App.tsx
+++ src/App.tsx
@@ -25,41 +25,44 @@ function App() {
   }
 
   if (auth.error) {
-    console.log("err:",auth.error.message)
-    console.log("err:",auth.error.name)
-    console.log("err:",auth.error.stack)
+    console.log("err:", auth.error.message)
+    console.log("err:", auth.error.name)
+    console.log("err:", auth.error.stack)
     return <div>Encountering error... {auth.error.message}</div>;
   }
 
   if (auth.isAuthenticated) {
-    console.log("authenticated",auth.isAuthenticated)
+    console.log("authenticated", auth.isAuthenticated)
     return (
-      <div>
-        <pre> Hello: {auth.user?.profile.email} </pre>
-        <pre> ID Token: {auth.user?.id_token} </pre>
-        <pre> Access Token: {auth.user?.access_token} </pre>
-        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <pre> Hello: {auth.user?.profile.email} </pre>
+          <pre> ID Token: {auth.user?.id_token} </pre>
+          <pre> Access Token: {auth.user?.access_token} </pre>
+          <pre> Refresh Token: {auth.user?.refresh_token} </pre>
 
-        <button onClick={() => auth.removeUser()}>Sign out</button>
-      </div>
+          <button onClick={() => auth.removeUser()}>Sign out</button>
+        </div>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/dashbord" element={<DashboardPage />}></Route>
+          <Route path="/expenseList" element={<ExpenseListPage />}></Route>
+          <Route path="/expenseList/expenseFrom/:id" element={<ExpenseFormPage />}></Route>
+          <Route path="/logout" element={<LogoutPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     );
   }
   return (
 
-    <BrowserRouter>
-      <Header />
-      <div>
-        <button onClick={() => auth.signinRedirect()}>Sign in</button>
-        <button onClick={() => signOutRedirect()}>Sign out</button>
-      </div>
-      <Routes>
-        <Route path="/" element={<LoginPage />}></Route>
-        <Route path="/dashbord" element={<DashboardPage />}></Route>
-        <Route path="/expenseList" element={<ExpenseListPage />}></Route>
-        <Route path="/expenseList/expenseFrom/:id" element={<ExpenseFormPage />}></Route>
-        <Route path="/logout" element={<LogoutPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+
+
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
+    </div>
+
   )
 }
 
