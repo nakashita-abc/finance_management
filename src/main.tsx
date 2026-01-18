c9aa2285e9f4d863fb0dba6dc341ea01050796c5
nakasita <kosei0224.loop@icloud.com>
2026-01-18 21:11:25 +0900

    env fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import { AuthProvider } from "react-oidc-context";
 import { ProfileProvider } from "./providers/ProfileProvider.tsx"
 
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_HUYvDB3aW",
-  client_id: "22omuoadoaf3b2t1in154vjn9e",
-  redirect_uri: "http://localhost:5173/",
+  authority: import.meta.env.VITE_COGNITO_AUTHORITY,
+  client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_REDIRECT_URI,
   response_type: "code",
   scope: "email openid phone",
 };
